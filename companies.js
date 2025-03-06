8802ac531b6efd467c6a0d2857fa565dff1fceb1
User8395 <therealuser8395@proton.me>
2025-03-06 16:31:02 -0500

    add nw mainline rail

---
+++ companies.js
@@ -0,0 +1,12 @@
+const companies = {
+    "northern": {
+        name: "Northern Railway Association",
+        img: "northern.png",
+        translateY: 100
+    },
+    "northwest": {
+        name: "North Western Mainline Rail",
+        img: "northwestern.png",
+        translateY: 100
+    }
+}
